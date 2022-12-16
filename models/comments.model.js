46aa5c69e073180c634b945512a59c3613663a9f
Tayamul Rai <Tayamul@Bhumikas-MacBook-Pro.local>
2022-12-16 15:24:20 +0000

    Completes ticket 12

--- models/comments.model.js
+++ models/comments.model.js
@@ -17,14 +17,6 @@ const dropComments = (comment_id) => {
       const queryString = `DELETE FROM comments WHERE comment_id = $1;`;
       return db.query(queryString, [comment_id]);
     });
-
-  // const queryString = `DELETE FROM comments WHERE comment_id = $1`;
-  // return db.query(queryString, [comment_id]).then(({ rows }) => {
-  //   // if (rowCount === 0) {
-  //   //   return Promise.reject({ status: 400, msg: "Bad Request" });
-  //   // }
-  //   return rows;
-  // });
 };
 
 module.exports = { dropComments };
